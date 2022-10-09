e911009e6b81b3b5561b8f0701149c7b1c23fce4
npacadmin <greenshirt82@gmail.com>
2022-10-09 13:45:28 -0400

    Added Skipper's email to the "to" line.

--- SS_Email_Functions.py
+++ SS_Email_Functions.py
@@ -27,7 +27,7 @@ logger.addHandler(file_handler)
 ###	This module handles all the emailing functions.
 ###
 ###	The primary objective is to send the monthly reports to the Treasurer on the 
-###		first use/opening of the app in a month.
+###		first use/opening of the app in a month.  Note -- this does not work yet
 ###
 ###	The secondary objective is to send email notifications from the app when a Skipper
 ###		completes a sail, and to the Commodore when a sail has started/ended.
@@ -38,8 +38,10 @@ def send_email(em_address, em_subject, em_body):
 	msg = EmailMessage()
 	msg.set_content(em_body)
 	msg['Subject'] = em_subject
-	msg['From'] = 'NPSC_Sailor@clubhousepc'
+	msg['From'] = 'npsc.sailor@gmail.com'
 	msg['To'] = em_address
+	msg['Bcc'] = 'comm@navypaxsail.com, treas@navypaxsail.com'
+
 
 	s = smtplib.SMTP('localhost')
 	s.send_message(msg)
@@ -64,26 +66,38 @@ def sailplan_closed_email(sailplan_df):
 	# sailplan_df[12] = sp_billmembers int
 	# sailplan_df[13] = sp_completed int
 
+	db = sqlite3.connect('Sailsheets.db')
+	c = db.cursor()
+
+	c.execute("SELECT * FROM Members WHERE m_id = :mid", {'mid': sailplan_df[2],})
+	memberdetails = c.fetchone()
+	myskipper_email = memberdetails[6].lower()
+
+	db.commit()
+	db.close()
+
 	mytitle = 'This is not a bill.  Do not click any links that may be in this email.' + '\n' + '\n'
 
-	myheader = 'The following sailplan has completed: ' + str(sailplan_df[0]) + '\n' + '\n'
+	myheader = 'The following sailplan was completed: ' + str(sailplan_df[0]) + '\n' + '\n'
 
-	myskipper = 'Skipper ID: ' + str(sailplan_df[2]) + '\n' + '\n'
+	myskipper = 'Skipper ID: ' + '\t' + '\t' + str(sailplan_df[2]) + '\n' 
+	myskipper_name = 'Skipper Name: ' + str(memberdetails[1]) + '\n' + '\n'
 	myboat = 'Sailboat: ' + sailplan_df[3] + '\n'
 	mypurpose = 'Purpose: ' + sailplan_df[4] + '\n'
 	mydescription = 'Description: ' + sailplan_df[5] + '\n'
-	mybillmembers = 'Members onboard boat: ' + str(sailplan_df[12]) + '\n' + '\n'
+	mybillmembers = 'Persons onboard boat: ' + str(sailplan_df[12]) + '\n' + '\n'
 	mytimeoutin = 'Times out/in: ' + sailplan_df[1] + ' / ' + sailplan_df[7] + '\n'
 	myhours = 'Hours used: ' + str(sailplan_df[8]) + '\n'
-	myfee_each = 'Fee each: $ ' + str(sailplan_df[9]) + '\n'
-	myfeesdue = 'Fees due NPSC: $ ' + str(sailplan_df[10]) + '\n'
-	mymwrbill = 'Payable to MWR for this sail: $ ' + str(sailplan_df[11]) + '\n'
+	myfee_each = 'Fee for each person on the boat: $ ' + str(sailplan_df[9]) + '\n' + '\n'
+	myfeesdue = 'Fees due NPSC: $ ' + str(sailplan_df[10]) + '\n' + '\n'
+	mymwrbill = 'Payable to MWR by NPSC for this sail: $ ' + str(sailplan_df[11]) + '\n'
 
 
-	myemailbody = mytitle + myheader + myskipper + myboat + mypurpose + mydescription + mybillmembers + mytimeoutin + myhours + myfee_each + myfeesdue + mymwrbill
+	myemailbody = mytitle + myheader + myskipper + myskipper_name + myboat + mypurpose + mydescription + mybillmembers + mytimeoutin + myhours + myfee_each + myfeesdue + mymwrbill
 
-	send_email('greenshirt82@gmail.com', 'Closed Sailplan', myemailbody)
-	send_email('comm@navypaxsail.com', 'Closed Sailplan #: ' + str(sailplan_df[0]), myemailbody)
+	send_email(myskipper_email, 'Closed Sailplan #: ' + str(sailplan_df[0]), myemailbody)
+	#send_email('npsc.sailor@gmail.com', 'Closed Sailplan #: ' + str(sailplan_df[0]), myemailbody)
+	#send_email('comm@navypaxsail.com', 'Closed Sailplan #: ' + str(sailplan_df[0]), myemailbody)
 
 	return
 
@@ -97,4 +111,4 @@ def member_email(ledgerid, sailplanid):
 	return
 
 if __name__ == '__main__':
-    send_email('greenshirt82@gmail.com', 'Test email subject', 'This is just a test of the smtp library for Sailsheets.')
+    send_email('npsc.sailor@gmail.com', 'Test email subject', 'This is just a test of the smtp library for Sailsheets.')
